b1b0afa26567cdb83b149158516326def69ba76c
Jason Suttles <jasonssuttles@gmail.com>
2026-02-03 22:25:15 -0500

    Chore: Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 
 export default defineConfig({
+  base: '/shadow-siege/',
   plugins: [
     checker({
       typescript: true,
